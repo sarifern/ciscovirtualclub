76a72d62def40e72a3c1fc019a84d77051896062
Sari Fernandez <sarifern@cisco.com>
2019-07-10 16:10:16 -0500

    Changes for Whitenoise v4.0

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
