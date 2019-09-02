a62333c798f6d6ce58fa58cb4009d408d5a4593a
Sari Fernandez <sarifern@cisco.com>
2019-09-02 17:46:12 -0500

    update to models to include Team and Profile

--- ic_marathon_app/admin.py
+++ ic_marathon_app/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Workout
+from .models import Workout, Team, Profile
 # Register your models here.
+
+
 class WorkoutAdmin(admin.ModelAdmin):
-    list_display=('uuid',)
+    list_display = ('uuid', 'belongs_to')
     list_filter = ['uuid']
 
-admin.site.register(Workout,WorkoutAdmin)
+
+admin.site.register(Workout, WorkoutAdmin)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_name', 'team_distance')
+    list_filter = ['team_name']
+
+
+admin.site.register(Team,TeamAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','cec', 'distance','team')
+    list_filter = ['user','cec','team']
+
+
+admin.site.register(Profile,ProfileAdmin)
--- ic_marathon_app/models.py
+++ ic_marathon_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import uuid
 from .validators import validate_file_size
 # Create your models here.
@@ -13,3 +16,40 @@ class Workout(models.Model):
     distance = models.DecimalField(
         default=0.00, max_digits=4, decimal_places=2)
     photo_evidence = models.ImageField(validators=[validate_file_size])
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+    team_distance = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.team_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cec = models.CharField(max_length=30)
+    distance = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2)
+    RUNNER = "Runner"
+    BIKER = "Biker"
+    CATEGORY_CHOICES = (
+        (None, 'Please select a category'),
+        (RUNNER, 'Runner'),
+        (BIKER, 'Biker'),
+    )
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    team = models.OneToOneField(
+        Team, related_name="team", default=None, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
