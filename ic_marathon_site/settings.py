ce969203b478d48cb5becb4741f7e459a368b36d
Sari Fernandez <sarifern@cisco.com>
2019-11-03 11:58:26 -0600

    logging for django heroku

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -206,3 +206,19 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'eu-west-1'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
