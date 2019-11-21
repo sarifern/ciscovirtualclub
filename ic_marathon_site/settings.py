362e2ed24bf28b4c529bfbaced94d7b2e1d31612
Sari Fernandez <sarifern@cisco.com>
2019-11-21 16:02:47 -0600

    s3 settings wont change because it is a prod file duh

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -190,11 +190,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-'''
+
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-'''
+
 DEFAULT_FILE_STORAGE = 'ic_marathon_site.storage_backends.MediaStorage'
 
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('FB_APP_KEY')        # App ID
