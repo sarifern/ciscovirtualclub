bb92e3ac1564223f9fe6e0fdc183c5fb402ce667
Sari Fernandez <sarifern@cisco.com>
2019-12-09 20:46:48 -0600

    adding photo_evidence to WorkoutForm (oops)

--- ic_marathon_app/models.py
+++ ic_marathon_app/models.py
@@ -76,7 +76,7 @@ class Workout(models.Model):
 class WorkoutForm(ModelForm):
     class Meta:
         model = Workout
-        fields = ['distance', 'date_time', 'time']
+        fields = ['distance', 'date_time', 'time','photo_evidence']
         widgets = {
             'date_time': DateTimePickerInput(),
             'time': TimePickerInput(),
