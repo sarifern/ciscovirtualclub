63a107e89d89fa90e51b133826d19543990042aa
Sari Fernandez <sarifern@cisco.com>
2020-11-10 13:34:26 -0600

    [sarifern] debug_pref var

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG_PREF')
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
