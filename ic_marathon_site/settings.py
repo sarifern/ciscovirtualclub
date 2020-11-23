9dd17b1b2cc34676af68fea7cf1705c3716f7741
Sari Fernandez <sarifern@cisco.com>
2020-11-23 07:19:30 -0600

    [sarifern] DEBUG_PREF in settings

--- ic_marathon_site/settings.py
+++ ic_marathon_site/settings.py
@@ -22,10 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_PREF')
+if os.environ.get('DEBUG_PREF')!='False':   
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
